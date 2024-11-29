dad529ac0fe5d117da5e720355ea45d6e18716a1
Maria-Paula23 <132026592+Maria-Paula23@users.noreply.github.com>
2024-11-29 14:02:38 +0000

    Finalizado

---
+++ perguntas.js
@@ -0,0 +1,35 @@
+criaCartao(
+    'Programação',
+    'O que é HTML?',
+    'O HTML é uma lnguagem de marcação.'
+)
+
+criaCartao(
+    'Matemática',
+    'O que é um triângulo equilátero?',
+    'É um triângulo onde os lados são todos iguais, ou seja, de mesmo tamanho.'
+)
+
+criaCartao(
+    'Filmes',
+    'Qual é o filme com maior bilheteria do mundo?',
+    'Avatar.'
+)
+
+criaCartao(
+    'Educação Financeira',
+    'O que é educação financeira?',
+    'Educação financeira compreende uma série de conhecimentos, habilidades e atitudes para gerir bem as finanças.'
+)
+
+criaCartao(
+    'Matemática',
+    'Quem é considerado o príncipe da matemática?',
+    'Friedrich Gauss.'
+)
+
+criaCartao(
+    'Português',
+    'O que é substantivo?',
+    'Substantivo é a palavra a que da nome as coisas.'
+)
